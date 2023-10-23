3a026acee9ef9419dd7df9b65124ab7772834da8
Sridhar <sridhar.krishnan1202@gmail.com>
2023-10-23 13:10:40 +0530

    understanding views, models, migrations, admin panel, display data from model->view->template

--- taskmate/toDoList/models.py
+++ taskmate/toDoList/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+# 1- this is our first model, this is how we create models, we derived class TodoList that inherits models.Model, that models from django.db then use Model class from models
+    
+# 2 - then we need mention those fields that are applicable for the particular model, here for todo we have task column, status column with the datatype & their length
+
+# 3 - then we need to follow migrations that we follow while start of the django, that is the two commands, see the django_ref_pad for more clarififcation 
+    # 1 - python manage.py makemigrations - this will help us to convert our queries - create our migrations for the particular model, it will create a migrations file for this class TodoList inside the migratrions folder
+    # 2 - python manage.py migrate - this will help us to actually apply the queries - this will apply our migration queries
+    # 3 - whenever we change any columns, add columns, alter datatypes, we need run above 2 commands to apply changes
+    
+# 4 - after run these two commands there nothing going to change we just create migrations for our TodoList class and apply the migration queries, finally to see changes we need register them in admin.py file - this file is register all the models that we are created
+
+# IMPORTANT thing its not a mandatory, its jus your wish if you need showcase your model in admin panel then you need to register them otherwise if you want to hide them, hide means not a important one no need to display,
+
+# 5 - next how we register ?
+    # 1 - you need import your model in admin.py, then
+    # 2 - regsiter usgin - admin.site.register(pass the model name)
+    
+# 6 - all done, now you may able see your TodoList model and able to add todolists and one thing to remember after created todo's it always appearing in object type, why becuase our model TodoList is class, every element we are going to register will going to store as a objects, to rectify that use __str__ special function to get the string 
+
+# need to look up the django model field types
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#model-field-types
+
+class TodoList(models.Model):
+    task = models.CharField(max_length=300) # CharField are datatype that are pre-definedly available under the django models, max_length is a argument for the CharField
+    status = models.BooleanField(default=False) # BooleanField are datatype that are pre-definedly available under the django models
+    
+    def __str__(self):
+        done = 'Done' if self.status else 'Yet to be done' # ternary opertor in python, if true then the lefthandside as result else the else block
+        return '{0} - {1}'.format(self.task,done)
+    
--- taskmate/toDoList/views.py
+++ taskmate/toDoList/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from toDoList.models import TodoList
+import sys
 
 # Create your views here.
 # def toDoList(request):
 #     return HttpResponse('Your To Do\'s are Here, Glad to Have you here!')
 
 def toDoList(request):
-    datas = {'data': 'Warm Welcome from To Do Page!'}
+    datas = {'welcome_text': 'Warm Welcome from To Do Page!', 'data':TodoList.objects.all} # get all objects from model to view, import which to retrieve data with use of objects.all method, this method return data as List datatype
     return render(request,'todo.html',datas)
 
 def taskList(request):
@@ -21,8 +23,3 @@ def contactList(request):
 def aboutUs(request):
     datas = {'data': 'Warm Welcome from About Page!'}
     return render(request,'about.html',datas)
-
-# this for reference purpose, <todo.html> jinja2 stuffs
-# we get dynamic datas from views to templates using jinja2 method, jinja2 used to iterate datas, extendinng templates (which means template inheritance), integrating links refer google to know more about jinja2
-# {%comment %} {% extends "footer.html" %} - in template we may able to inherit single template in another template file {%endcomment %} 
-# {% comment %} whatever we give inside the block endblock it will replace the block endblock in the base template file - here we use it for title of the webpage, but we may able to use block endblock with any kind of stuff {%endcomment %} 
